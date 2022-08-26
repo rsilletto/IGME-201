d68629a6d837904eaca67039b8d2fed4fb165133
rsilletto <111778654+rsilletto@users.noreply.github.com>
2022-08-25 23:48:48 -0400

    Silletto_PE1_HelloWorld

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/* Rose Silletto
+*  IGME-201 PE1 Hello World
+*  August 25, 2022
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");
+            Console.WriteLine("Rose Silletto");
+
+            int x = 5;
+            int y = 4;
+
+            int result = x + y;
+
+            Console.WriteLine("\n");
+            Console.WriteLine("result = " + result);
+
+        }
+    }
+}
