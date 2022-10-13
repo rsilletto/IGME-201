a39d4151e2c751891b8688de5e01c039c6e8361e
rsilletto <111778654+rsilletto@users.noreply.github.com>
2022-10-12 22:41:55 -0400

    PE 11
    PE 11 Defining Classes

---
+++ PE 11 Traffic/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Rose Silletto
+ * IGME 201
+ * PE 11 #6
+ * Oct. 6 2022
+ */
+
+using System.Transactions;
+
+namespace Traffic
+{
+    public class Program
+    {
+        public void Main(String[] args)
+        {
+
+        }
+    }
+
+}
---
+++ Vehicles_2/Pickup.cs
@@ -0,0 +1,21 @@
+﻿/* Rose Silletto
+ * IGME 201
+ * PE 11 Class Structures
+ * Oct. 11 2022
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles_2
+{
+    public class Pickup : Car
+    {
+
+    }
+
+
+}
