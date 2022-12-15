e8a6f74ec67badbd59ca26ac211c9268514f27a2
rsilletto <111778654+rsilletto@users.noreply.github.com>
2022-12-14 22:52:50 -0500

    Final Exam

---
+++ FinalExam_1/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Rose Silletto
+ * IGME 201
+ * Final Exam #1
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam_1
+{
+    public class MyStack
+    {
+        public List<int> myStack = new List<int>();
+
+        public void Push(int x)
+        {
+            myStack.Add(x);
+        }
+
+        public int? Pop()
+        {
+            if (myStack.Count > 0)
+            {
+                int? r = myStack[myStack.Count + 1];
+                myStack.RemoveAt(myStack.Count - 1);
+
+                return r;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public int? Peek()
+        {
+            if (myStack.Count > 0)
+            {
+                return myStack[myStack.Count - 1];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ FinalExam_2/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Rose Silletto
+ * IGME 201
+ * Final Exam #2
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam_1
+{
+    public class MyQueue
+    {
+        public List<int> myQueue = new List<int>();
+
+        public void Enqueue(int x)
+        {
+            myQueue.Insert(0, x);
+        }
+
+        public int? Dequeue()
+        {
+            if (myQueue.Count > 0)
+            {
+                int? r = myQueue[0];
+                myQueue.RemoveAt(0);
+
+                return r;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public int? Peek()
+        {
+            if (myQueue.Count > 0)
+            {
+                return myQueue[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
